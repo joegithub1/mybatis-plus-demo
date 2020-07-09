a6c6a61ef2ff9548530e9c9f78ebd8ec513c4942
hj <day_1111@126.com>
2020-07-09 11:08:18 +0800

    mybatis-plus 配置修改

--- src/main/java/com/exampl/MybatisPlusDemoApplication.java
+++ src/main/java/com/exampl/MybatisPlusDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@MapperScan(value = {"com.exampl"})
+@MapperScan(value = {"com.exampl.mapper"})
 @SpringBootApplication
 public class MybatisPlusDemoApplication {
 
